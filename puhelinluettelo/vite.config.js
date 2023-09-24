4f46b0413446e0da2317b47a805e559db675d56c
YuMZyX <joni1koskinen@gmail.com>
2023-09-24 21:13:56 +0300

    vite.config määrittelyä muutettu

--- puhelinluettelo/vite.config.js
+++ puhelinluettelo/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+
 export default defineConfig({
   plugins: [react()],
 
@@ -10,6 +12,9 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     }
   },
